4ce71264d52030f0ef14816a0b723c48fee10133
Tuấn Hoàng <hdtuan87@gmail.com>
2018-11-26 21:26:30 +0700

    update activity list item

--- app/src/main/java/com/tuanhd/minipos/MainActivity.kt
+++ app/src/main/java/com/tuanhd/minipos/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.tuanhd.minipos
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.tuanhd.minipos.listItem.ActivityListItem
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,5 +12,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btnAddItem.setOnClickListener {
+            showActivityAddItem()
+        }
+
+        btnListItem.setOnClickListener { showActivityListItem() }
+    }
+
+    private fun showActivityListItem() {
+        startActivity(Intent(this, ActivityListItem::class.java))
+    }
+
+    private fun showActivityAddItem() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
---
+++ app/src/main/java/com/tuanhd/minipos/listItem/ActivityListItem.kt
@@ -0,0 +1,13 @@
+package com.tuanhd.minipos.listItem
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.tuanhd.minipos.R
+
+class ActivityListItem : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_item)
+    }
+}
